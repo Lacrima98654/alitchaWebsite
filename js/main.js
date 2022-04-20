35c6ab8c2eaca2a3b34ae4b440ea4fa4ca786c4a
Lacrima98654 <romeokakpo3@gmail.com>
2022-04-20 10:32:00 +0100

    Réfactoring et fixation de bug

--- js/main.js
+++ js/main.js
@@ -2,7 +2,32 @@
 
 	"use strict";
 
+// Initiate the wowjs
+new WOW().init();
 
+
+// Sticky Navbar
+$(window).scroll(function () {
+	if ($(this).scrollTop() > 45) {
+		$('.navbar').addClass('sticky-top shadow-sm');
+	} else {
+		$('.navbar').removeClass('sticky-top shadow-sm');
+	}
+});
+
+
+// Back to top button
+$(window).scroll(function () {
+	if ($(this).scrollTop() > 100) {
+		$('.back-to-top').fadeIn('slow');
+	} else {
+		$('.back-to-top').fadeOut('slow');
+	}
+});
+$('.back-to-top').click(function () {
+	$('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+	return false;
+});
   // Form
 	var contactForm = function() {
 		if ($('#contactForm').length > 0 ) {
